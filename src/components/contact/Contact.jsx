be8ca0b4ebdf58033c35eb3154714dec98115203
simon-jk-casey <jesusiwasevil@gmail.com>
2018-09-23 15:44:20 +1200

    basic contact component, without mailform, removed unneeded constuctor

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,11 +1,6 @@
 import React, { Component } from 'react'
 
 export default class Contact extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {}
-  }
-
   render () {
     return (
       <div id='contactWrapper'>
