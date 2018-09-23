14a6bd4925753cc2147f4c334d848cb7acf90515
simon-jk-casey <jesusiwasevil@gmail.com>
2018-09-23 14:49:48 +1200

    set folder structure, add unstyled banner component

---
+++ src/components/banner/Banner.jsx
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+export default class Banner extends Component {
+  render () {
+    return (
+      <header>
+        <div id='navLeft'>
+          <button>Home</button>
+          <button>Philosophy</button>
+          <button>Services</button>
+        </div>
+        <div id='navMid'>
+          <img src={require('../../assets/images/hwav_logo.jpg')} />
+        </div>
+        <div id='navRight'>
+          <button>Testimonials</button>
+          <button>Gallery</button>
+          <button>Contact</button>
+        </div>
+      </header>
+    )
+  }
+}
