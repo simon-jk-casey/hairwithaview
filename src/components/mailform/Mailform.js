d4e7d6b10b4ea01ae4ffd5c509056e81f86faaf7
simon-jk-casey <jesusiwasevil@gmail.com>
2018-11-11 15:34:39 +1300

    form sends to endpoint success, state toggles and sending/success component view changes working - unsecured url removed

--- src/components/mailform/Mailform.js
+++ src/components/mailform/Mailform.js
@@ -1,4 +1,25 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+
+class Sending extends Component {
+  render () {
+    return (
+      <div>
+        <h2>...SENDING...</h2>
+      </div>
+    )
+  }
+}
+
+class Success extends Component {
+  render () {
+    return (
+      <div>
+        <p>Thanks for your message!  We will be in touch soon.</p>
+      </div>
+    )
+  }
+}
 
 export default class Mailform extends Component {
   constructor (props) {
@@ -6,64 +27,119 @@ export default class Mailform extends Component {
     this.state = {
       name: '',
       email: '',
-      message: ''
+      message: '',
+      sending: false,
+      sendSuccess: false,
+      sendError: false
     }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  handleChange (event) {
-    this.setState({ [event.target.name]: event.target.value })
-    console.log(this.state)
+  handleChange (e) {
+    this.setState({ [e.target.name]: e.target.value })
+    console.log(this.state, 'input change')
   }
 
-  handleSubmit () {
-    alert("Message sent!")
-    this.setState(
-      {
-        name: '',
-        email: '',
-        message: ''
-      }
-    )
-    console.log(this.state)
+  toggleSending () {
+    this.setState(prevState => ({
+      sending: !prevState.sending
+    }), () => console.log(this.state, 'toggle test'))
   }
+
+  toggleSendSuccess () {
+    this.setState(prevState => ({
+      sending: !prevState.sending,
+      sendSuccess: !prevState.sendSuccess
+    }), () => console.log(this.state.sendSuccess, 'sent'))
+  }
+
+  toggleSendError () {
+    this.setState(prevState => ({
+      sending: !prevState.sending,
+      sendError: !prevState.sendError
+    }))
+  }
+
+  handleSubmit (e) {
+    this.toggleSending()
+    e.preventDefault()
+    e.target.reset()
+    const data = {
+      name: this.state.name,
+      email: this.state.email,
+      content: this.state.message
+    }
+
+    axios.post(``, data) // removed URL while unsecured
+      .then((response) => {
+        if (response.status === 200) {
+          this.toggleSendSuccess()
+        }
+        console.log(response.status, 'axios send response status')
+      })
+      .catch((err) => {
+        console.log(err, 'axios send error')
+      })
+
+    // alert('Message sent!')
+    // this.setState(
+    //   {
+    //     name: '',
+    //     email: '',
+    //     message: ''
+    //   }
+    // )
+    console.log(this.state, 'check state clear:')
+  }
+
+  // try an async/await version of handlesubmit
 
   render () {
-    return (
-      <form>
-        <div>
-          <label htmlFor='nameField'>Name*</label>
-          <input
-            type='text'
-            name='name'
-            onChange={event => this.handleChange(event)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor='email'> Email*</label>
-          <input
-            type='email'
-            name='email'
-            onChange={event => this.handleChange(event)}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor='message'> Message*</label>
-          <textarea
-            rows='10'
-            cols='50'
-            name='message'
-            onChange={event => this.handleChange(event)}
-            required
-          />
-        </div>
-        <div>
-          <button onClick={this.handleSubmit.bind(this)}>Send Message</button>
-          <input type='submit' value='Send Message' />
-        {/* NOTE: button with submithandler does not enforce required field; input however does (though not if using submit handler => look into handling required fields ) */}
-        </div>
-      </form>
-    )
+    if (this.state.sending) {
+      return (
+        <Sending />
+      )
+    } else if (this.state.sendSuccess) {
+      return (
+        <Success />
+      )
+    } else {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label htmlFor='nameField'>Name</label>
+            <input
+              type='text'
+              name='name'
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor='email'> Email</label>
+            <input
+              type='email'
+              name='email'
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+          <div>
+            <label htmlFor='message'> Message</label>
+            <textarea
+              rows='10'
+              cols='50'
+              name='message'
+              onChange={this.handleChange}
+              required
+            />
+          </div>
+          <div>
+            <input type='submit' value='Send Message' />
+          </div>
+        </form>
+      )
+    }
   }
 }
