12e1423b4badeebbc060782cd5bfc80fd24c558e
simon-jk-casey <jesusiwasevil@gmail.com>
2019-07-08 11:26:25 +1200

    BACKGROUND IMG/OPACITY ADDED MOBILE STYLE COMPLETE

--- src/App.js
+++ src/App.js
@@ -17,24 +17,26 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
-        <Hamburger />
-        <Banner />
-        <MenuBar />
-        <div className='content'>
-          <SalonScroller />
-        </div>
-        <div className='mainBodyWrapper'>
-          <div className='sideSpacer'/>
-          <div className='mainBodyContent'>
-            <div id='about' className='content'><About /></div>
-            <div id='gallery' className='content'><Gallery /></div>
-            <div id='testimonials' className='content'><Testimonials /></div>
-            <div id='contact' className='content'><Contact /></div>
-            <div id='location' className='content'><Location /></div>
+        <div className='opaqueWrapper'>
+          <Hamburger />
+          <Banner />
+          <MenuBar />
+          <div className='content'>
+            <SalonScroller />
           </div>
-          <div className='sideSpacer'/>
+          <div className='mainBodyWrapper'>
+            <div className='sideSpacer'/>
+            <div className='mainBodyContent'>
+              <div id='about' className='content'><About /></div>
+              <div id='gallery' className='content'><Gallery /></div>
+              <div id='testimonials' className='content'><Testimonials /></div>
+              <div id='contact' className='content'><Contact /></div>
+              <div id='location' className='content'><Location /></div>
+            </div>
+            <div className='sideSpacer'/>
+          </div>
+          <Footer />
         </div>
-        <Footer />
       </div>
     )
   }
