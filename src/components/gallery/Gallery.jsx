a4015928e7b88ca0a99b6da3c4999421534e2f56
simon-jk-casey <jesusiwasevil@gmail.com>
2019-07-14 17:53:13 +1200

    corrected css problems in gallery all devices

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -77,7 +77,7 @@ export default class Gallery extends Component {
   render() {
     return (
       <div className='gallery'>
-      <h2>Gallery</h2>
+      <h2 classname='galleryHeader'>Gallery</h2>
       <div className='galleryWrapper'>
         <div className='buttonWrapperLeft'>
           <FontAwesomeIcon
