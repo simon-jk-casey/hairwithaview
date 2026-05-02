3ff4de58b279cf68f17b75702549401b781fd5c2
Simon Casey <jesusiwasevil@gmail.com>
2026-05-02 14:12:08 +1200

    widget add

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -12,7 +12,8 @@ export default class Gallery extends Component {
       <div className='gallery'>
         <h2 className='galleryHeader'>Gallery</h2>
         <div className='galleryWrapper'>
-          <iframe src="https://widgets.tagembed.com/163955?view" style={{width:'100%', height:'500px', overflow: 'auto'}} frameBorder="0" allowtransparency="true"></iframe>   
+          <iframe src="https://widgets.commoninja.com/iframe/97565379-f620-44f4-a055-ed4623edee2b" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>
+          {/* <iframe src="https://widgets.tagembed.com/163955?view" style={{width:'100%', height:'500px', overflow: 'auto'}} frameBorder="0" allowtransparency="true"></iframe>    */}
         </div>
       </div>
     )
